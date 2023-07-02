5c74d786b115c59ca363d27bf2fa01ec64dc7f86
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-07-02 08:05:37 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -17,11 +17,11 @@ const contactsSlice = createSlice({
       reducer(state, action) {
         state.items.push(action.payload);
       },
-    },
-    prepare(newContact) {
-      return {
-        payload: { id: nanoid(), ...newContact },
-      };
+      prepare(newContact) {
+        return {
+          payload: { id: nanoid(), ...newContact },
+        };
+      },
     },
     removeContact(state, action) {
       const index = state.items.findIndex(
