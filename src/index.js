f9df70fdae11e1e2ff2f0b1b4304527c873d5c79
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-07-05 17:17:51 +0300

    update package.json

--- src/index.js
+++ src/index.js
@@ -15,17 +15,4 @@ createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
-// import React from 'react';
-// import './index.css';
-// import App from './components/App';
-// import { Provider } from 'react-redux';
-// import { createRoot } from 'react-dom/client';
-// import { store } from './redux/store';
 
-// createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
