137fc74e016a6ccc4304a0de54786060dc10ac6c
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-07-09 11:11:59 +0300

    update css

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -69,9 +69,6 @@ const ContactForm = () => {
           value={number}
           onChange={handleChange}
           id={contactNumberId}
-          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          // required
         />
       </label>
 
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectVisibleContacts } from '../../redux/selectors';
 import { deleteContact } from '../../redux/operations';
+import style from './ContactList.module.css';
 
 export default function ContactList() {
   const contacts = useSelector(selectVisibleContacts);
   const dispatch = useDispatch();
 
   return (
-    <ul>
+    <ul className={style.list}>
       {contacts.map(({ id, name, phone }) => (
-        <li key={id}>
+        <li key={id} className={style.item}>
           {name + ' : ' + phone}
           <button
+            className={style.button}
             type="button"
             name="delete"
             onClick={() => dispatch(deleteContact(id))}
