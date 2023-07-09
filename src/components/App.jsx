a215497098ab10c971b908f57a0ec3d10ffcc85b
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-07-09 13:25:00 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import { fetchAll } from '../redux/operations';
 import { selectContact } from '../redux/selectors';
+import style from '../app.css/app.module.css';
 
 const App = () => {
   const contacts = useSelector(selectContact);
@@ -15,9 +16,9 @@ const App = () => {
   }, [dispatch]);
   return (
     <>
-      <h1>Phonebook</h1>
+      <h1 className={style.h1}>Phonebook</h1>
       <ContactForm />
-      <h2>Contacts</h2>
+      <h2 className={style.h2}>Contacts</h2>
       <Filter />
       <ContactList contacts={contacts} />
     </>
