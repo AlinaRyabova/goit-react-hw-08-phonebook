94cef0ad27c34e34eebc8200beb6d5f7505f263a
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-07-09 16:36:47 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -21,16 +21,20 @@ const contactsSlice = createSlice({
         state.items = action.payload;
       })
       .addCase(addContact.rejected, (state, action) => {
+        state.isLoading = false;
         state.error = action.error.message;
       })
       .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
         state.items = [...state.items, action.payload];
         state.error = null;
       })
       .addCase(deleteContact.rejected, (state, action) => {
+        state.isLoading = false;
         state.error = action.error.message;
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
         state.items = state.items.filter(item => item.id !== action.payload.id);
         state.error = null;
       });
