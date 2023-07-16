8dbf901e22da7a1e7d84bcc6df33f081f5ac6204
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-07-16 18:16:51 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -38,7 +38,7 @@ export const RegisterForm = () => {
           type="email"
           name="email"
           placeholder="Введіть адресу електронної пошти"
-          pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+          // pattern="([A-z0-9_.-]{1,})@([A-z0-9_.-]{1,}).([A-z]{2,8})"
           title="Будь ласка, введіть дійсну адресу електронної пошти"
           required
         />
@@ -49,7 +49,7 @@ export const RegisterForm = () => {
           type="password"
           name="password"
           placeholder="Введіть пароль"
-          pattern="^[a-zA-Z0-9!@#$%^&*()-_=+`~[\]{}|:<>/?]+$"
+          // pattern="^[a-zA-Z0-9!@#$%^&*()\\_\\=+`~\\[\\]{}|:<>/?]+$"
           title="Пароль повинен містити тільки латинські літери (як великі, так і малі), цифри та інші символи"
           required
         />
